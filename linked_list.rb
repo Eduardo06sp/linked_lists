059120d1643c1b5c15c3a2e38c1e77004a3f5d2e
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-06-17 22:42:39 -0700

    Add append method that adds a new node at the end

--- linked_list.rb
+++ linked_list.rb
@@ -8,4 +8,23 @@ class LinkedList
   def initialize
     @head = nil
   end
+
+  def append(value)
+    new_node = Node.new(value)
+
+    if head.nil?
+      self.head = new_node
+    elsif head.next_node.nil?
+      head.next_node = new_node
+    else
+      tmp = head
+
+      until tmp.next_node.nil?
+        p 'UNTIL STATEMENT RUNS'
+        tmp = tmp.next_node
+      end
+
+      tmp.next_node = new_node
+    end
+  end
 end
